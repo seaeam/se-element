1ed9808800a6b8c6a9735de8e92bcf22209677f7
SeaMmMm <SeaMmMm@users.noreply.github.com>
2025-04-14 10:51:59 +0000

    deploy: 2fdac8135a16b6adec400a9332d055926e913c32

--- assets/chunks/Desc.oqDPNlfg.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,W as s,a as c,b as o,h as n}from"./framework.C3cU7JZf.js";const r={},a={style:{"max-width":"600px"}};function i(_,p){const e=s("se-alert");return o(),c("div",a,[n(e,{title:"With description",type:"success",description:"This is a description."})])}const l=t(r,[["render",i]]);export{l as default};
